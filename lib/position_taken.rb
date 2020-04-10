d6dd60f757526f9cf3219f2d3baaac283a673ef4
Richard Hanolt <rhanolt@gmail.com>
2020-04-10 01:29:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 # code your #position_taken? method here!
-def position_taken?
-
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " "
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
 end
